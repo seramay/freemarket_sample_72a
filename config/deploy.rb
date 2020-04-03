c47f6ec317eaa864e2f8c8245a55f6f76218b75f
yuya-kumazawa27 <61692101+yuya-kumazawa27@users.noreply.github.com>
2020-04-03 14:58:36 +0900

    test
    test2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,28 @@
-# config valid for current version and patch releases of Capistrano
-lock "3.12.1"
+# # config valid for current version and patch releases of Capistrano
+# lock "3.12.1"
 
-set :application, "freemarket_sample_72a"
-set :repo_url, "git@github.com:seramay/freemarket_sample_72a.git"
+# set :application, "freemarket_sample_72a"
+# set :repo_url, "git@github.com:seramay/freemarket_sample_72a.git"
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' 
+# set :rbenv_type, :user
+# set :rbenv_ruby, '2.5.1' 
 
-# どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/freemarket_sample_72a.pem']
+# # どの公開鍵を利用してデプロイするか
+# set :ssh_options, auth_methods: ['publickey'],
+#                   keys: ['~/.ssh/freemarket_sample_72a.pem']
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
+# # Unicornの設定ファイルの場所
+# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+# set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
+# # デプロイ処理が終わった後、Unicornを再起動するための記述
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:restart'
+#   end
+# end
