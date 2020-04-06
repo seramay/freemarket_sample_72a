33c0617982781c5246a6fb1608a30cd04c12d7d4
akane222 <akane222@users.noreply.github.com>
2020-04-06 17:30:09 +0900

    items/pay-add

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-  test "should get pay" do
-    get items_pay_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
