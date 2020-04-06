29ee207ee5a287c5401b5b81de1718a41926333b
seramay <lilina172ki@gmail.com>
2020-04-06 17:51:24 +0900

    fixed_route
    チームメンバーがresourcesで統一しているので修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'items/new'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: "users/sessions",
   }
+  resources :items, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
 end
