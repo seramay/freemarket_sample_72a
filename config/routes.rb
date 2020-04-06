be23943b417d361bdacb3fa375ecdba04a5c7d85
Sho50302 <shogo.s1226@docomo.ne.jp>
2020-04-06 17:45:22 +0900

    addresses-new-view

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: "users/sessions",
   }
+
   resources :items, only: [:new, :show] do
     collection do
       get 'pay'
     end
   end
+  resources :addresses, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
 end
