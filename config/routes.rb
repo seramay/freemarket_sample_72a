15a036bf565b5d10a829d9d5fd31a67e09ab43fe
akane222 <akane222@users.noreply.github.com>
2020-04-06 10:45:00 +0900

    mypage-setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'items/pay'
+  get 'mypages/show'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: "users/sessions",
