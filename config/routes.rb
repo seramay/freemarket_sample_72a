50e8db211a6a7a7da761c6559ce4acb37cf305d0
akane222 <akane222@users.noreply.github.com>
2020-04-06 14:56:39 +0900

    purchase-fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: "users/sessions",
   }
-  get 'items/pay'
+  resources :items do
+    collection do
+      get 'pay'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
 end
