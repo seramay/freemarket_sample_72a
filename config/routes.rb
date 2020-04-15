672fd91d166032cfc7616bd7f4c09172369155f6
Sho50302 <shogo.s1226@docomo.ne.jp>
2020-04-15 20:48:18 +0900

    サーバーサイド　モデル、ルートの編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,26 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: "users/sessions",
+    sessions: 'users/sessions'
   }
-
-
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+  
+  root "homes#top"
+  
   resources :items, only: [:new, :show]
-  resources :addresses, only: [:new, :create]
   resources :items do
     collection do
       get 'pay'
     end
   end
-
+  
   resources :mypages, only: [:show, :destroy]
-
   resources :cards, only: [:new, :create]
   resources :addresses, only: [:new, :create]
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "homes#top"
 end
