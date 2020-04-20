9505c9356ae2339ce898708fa8d0a164b27cc66d
seramay <lilina172ki@gmail.com>
2020-04-20 13:30:33 +0900

    商品削除機能の追記
    削除する際に同時に削除できなかった為追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :brand
   has_many :orders
-  has_many :item_images
+  has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
 
   validates_associated :item_images
